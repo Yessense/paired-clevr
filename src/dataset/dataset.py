b0b15549c83a2310bf87cb2c0a89e04dfeef16e0
yessense <Akorchemnyj@gmail.com>
2022-06-20 20:41:47 +0300

    Added model

---
+++ src/dataset/dataset.py
@@ -0,0 +1,75 @@
+import json
+import os
+
+import torch
+import torchvision.io.image
+from torch.utils.data import Dataset
+from torchvision.io import read_image
+import matplotlib.pyplot as plt
+
+
+class PairedClevr(Dataset):
+    def __init__(self, scenes_dir, img_dir, indices):
+        self.scenes_dir = scenes_dir
+        self.img_dir = img_dir
+        self.img_template = "{name}_{idx:06d}.png"
+        self.json_template = "scene_{idx:06d}.json"
+        self._size = len(os.listdir(self.img_dir)) // 2
+        self.features = ['shape', 'color', 'size', 'material', 'x', 'y']
+        self.features_size: int = 6
+        self.indices = indices
+
+    def __len__(self):
+        return len(self.indices)
+
+    def __getitem__(self, index):
+        idx = self.indices[index]
+
+        item = []
+        for name in ['img', 'pair']:
+            img_path = os.path.join(self.img_dir, self.img_template.format(name=name, idx=idx))
+            img = read_image(img_path, mode=torchvision.io.image.ImageReadMode.RGB) / 255
+            item.append(img)
+
+        json_path = os.path.join(self.scenes_dir, self.json_template.format(idx=idx))
+        with open(json_path) as json_file:
+            annotations = json.load(json_file)
+
+        exchange_labels = torch.zeros(self.features_size, dtype=bool)
+
+        obj1 = annotations[0]['objects'][0]
+        obj2 = annotations[1]['objects'][0]
+
+        if obj1['shape'] != obj2['shape']:
+            exchange_labels[0] = True
+        elif obj1['color'] != obj2['color']:
+            exchange_labels[1] = True
+        elif obj1['size'] != obj2['size']:
+            exchange_labels[2] = True
+        elif obj1['material'] != obj2['material']:
+            exchange_labels[3] = True
+        elif obj1['3d_coords'][0] != obj2['3d_coords'][0]:
+            exchange_labels[4] = True
+        elif obj1['3d_coords'][1] != obj2['3d_coords'][1]:
+            exchange_labels[5] = True
+        else:
+            raise ValueError(f'All features are the same {obj1}, {obj2}')
+        item.append(exchange_labels.unsqueeze(-1))
+
+        return item
+
+
+if __name__ == '__main__':
+    dataset = PairedClevr(scenes_dir='/home/yessense/projects/paired-clevr/src/dataset/data/scenes',
+                          img_dir='/home/yessense/projects/paired-clevr/src/dataset/data/images')
+    start_idx = 300
+    n_images = 2
+
+    plt.figure(figsize=(30, 20))
+    fig, ax = plt.subplots(n_images, 2)
+    for i in range(n_images):
+        img, pair, exchange_label = dataset[i + start_idx]
+        print(*[dataset.features[i] for i, exchange in enumerate(exchange_label) if exchange])
+        ax[i, 0].imshow(img.permute(1, 2, 0))
+        ax[i, 1].imshow(pair.permute(1, 2, 0))
+    plt.show()
---
+++ src/model/training.py
@@ -0,0 +1,124 @@
+import os.path
+import sys
+
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint, StochasticWeightAveraging
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader
+
+sys.path.append("..")
+from pytorch_lightning.loggers import WandbLogger
+
+from src.dataset.dataset import PairedClevr
+from src.model.scene_vae import ClevrVAE
+import pytorch_lightning as pl
+from argparse import ArgumentParser
+
+# ------------------------------------------------------------
+# Constants
+# ------------------------------------------------------------
+
+DEFAULT_SEED = 42
+DEFAULT_DATASET_PATH = './dataset/data/'
+DEFAULT_IMAGES_DIR_PATH = os.path.join(DEFAULT_DATASET_PATH, 'images')
+DEFAULT_SCENES_DIR_PATH = os.path.join(DEFAULT_DATASET_PATH, 'scenes')
+DEFAULT_LOGGER_DIR_PATH = '../dataset/'
+# ------------------------------------------------------------
+# Parse args
+# ------------------------------------------------------------
+
+parser = ArgumentParser()
+
+# add PROGRAM level args
+program_parser = parser.add_argument_group('program')
+
+# logger parameters
+program_parser.add_argument("--logger_dir", type=str, default=DEFAULT_LOGGER_DIR_PATH)
+program_parser.add_argument("--log_model", default=True)
+
+# dataset parameters
+program_parser.add_argument("--dataset_path", type=str, default=DEFAULT_DATASET_PATH)
+program_parser.add_argument("--images_path", type=str, default=DEFAULT_IMAGES_DIR_PATH)
+program_parser.add_argument("--scenes_path", type=str, default=DEFAULT_SCENES_DIR_PATH)
+
+# Experiment parameters
+program_parser.add_argument("--batch_size", type=int, default=4)
+program_parser.add_argument("--from_checkpoint", type=str, default='')
+program_parser.add_argument("--grad_clip", type=float, default=0.0)
+program_parser.add_argument("--seed", type=int, default=DEFAULT_SEED)
+
+# Add model specific args
+parser = ClevrVAE.add_model_specific_args(parent_parser=parser)
+
+# Add all the available trainer options to argparse#
+parser = pl.Trainer.add_argparse_args(parser)
+
+# Parse input
+args = parser.parse_args()
+
+# ------------------------------------------------------------
+# Logger
+# ------------------------------------------------------------
+
+wandb_logger = WandbLogger(project='paired-clevr', log_model=args.log_model, save_dir=args.logger_dir)
+
+# ------------------------------------------------------------
+# Load dataset
+# ------------------------------------------------------------
+
+dataset = PairedClevr(scenes_dir=args.scenes_path, img_dir=args.images_path)
+train, test = train_test_split(dataset, test_size=0.1, random_state=args.seed)
+train_loader = DataLoader(train, batch_size=args.batch_size, num_workers=2, shuffle=True)
+test_loader = DataLoader(test, batch_size=args.batch_size, num_workers=2)
+
+# ------------------------------------------------------------
+# Load model
+# ------------------------------------------------------------
+
+# model
+dict_args = vars(args)
+autoencoder = ClevrVAE(**dict_args)
+
+# ------------------------------------------------------------
+# Callbacks
+# ------------------------------------------------------------
+
+monitor = 'Total loss'
+
+# early stop
+patience = 5
+early_stop_callback = EarlyStopping(monitor=monitor, patience=patience)
+
+# checkpoints
+save_top_k = 2
+checkpoint_callback = ModelCheckpoint(monitor=monitor, save_top_k=save_top_k)
+
+# stohastic weight averaging
+swa_lrs = 1e-2
+swa = StochasticWeightAveraging(swa_lrs=swa_lrs)
+
+callbacks = [
+    checkpoint_callback,
+    # swa,
+    # early_stop_callback,
+]
+
+# ------------------------------------------------------------
+# Trainer
+# ------------------------------------------------------------
+
+# trainer parameters
+profiler = 'simple'  # 'simple'/'advanced'/None
+gpus = [args.gpus]
+log_every_n_steps = None
+
+# trainer
+trainer = pl.Trainer(gpus=gpus,
+                     max_epochs=args.max_epochs,
+                     profiler=profiler,
+                     callbacks=callbacks,
+                     logger=wandb_logger,
+                     gradient_clip_val=args.grad_clip)
+
+if not len(args.from_checkpoint):
+    args.from_checkpoint = None
+trainer.fit(autoencoder, train_dataloaders=train_loader, val_dataloaders=test_loader, ckpt_path=args.from_checkpoint)
